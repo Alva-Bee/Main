36744eb02fb0952f8849942be2fd982258671557
Gustavo Formagio <130837368+FmGustavo@users.noreply.github.com>
2025-09-18 11:47:00 +0000

    Instalação React (TS) e Tailwind

---
+++ alva/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-3xl font-bold">
+      Hello World 🚀
+    </div>
+  );
+}
+
+export default App;
+
